fa4f9d565f706a06a7e2a27383e1642e24a2f78f
SidsOnIt <feerthespeer@gmail.com>
2026-08-30 23:05:03 -0700

    updated scratch

--- src/ast.rs
+++ src/ast.rs
@@ -1,39 +1,22 @@
-use std::collections::HashMap;
 use indexmap::IndexMap;
-
-//unsure if this should be non-exhaustive or not, will have to research the current discourse around the age signal laws more
-pub enum Rating {
-    UnderOr13,  // 0 - 13
-    Rng14_15,   // 14 - 15
-    Rng16_17,   // 16 - 17
-    OverOr18,   // 18+
-}
-
-#[non_exhaustive]
-pub struct Meta {
-    title: String,
-    subtitle: String,
-    date: String,
-    authorship: Vec<String>,
-    tags: Vec<String>,
-    rating: Rating,
-}
+use std::collections::HashMap;
 
 #[non_exhaustive]
 pub enum TextStyle {
+    Regular,
     Bold,
     Italic,
     ULine,
     Strike,
-    Link(String),
+    Link(String)
 }
 
-pub struct Text {
+pub struct TextToken{
     style: TextStyle,
-    value: String,
+    value: String
 }
 
-pub struct TextSpan(Vec<Text>);
+pub struct TextBody(Vec<TextToken>)
 
 #[non_exhaustive]
 pub enum AlertStyle {
@@ -43,9 +26,9 @@ pub enum AlertStyle {
     Failure,
 }
 
-pub struct Alert {
+pub struct AlertToken {
     style: AlertStyle,
-    value: String,
+    body: TextBody,
 }
 
 #[non_exhaustive]
@@ -62,20 +45,26 @@ pub struct List {
     name: String,
     style: ListStyle,
     leaves: Vec<String>,
-    branches: Vec<Box<List>>,
+    branches: Vec<List>,
 }
 
 #[non_exhaustive]
-pub enum TreeStyle {
+pub enum LeafStyle {
     File,
     Folder,
+    Note,
+}
+
+#[non_exhaustive]
+pub struct Leaf {
+    style: LeafStyle,
+    value: String,
 }
 
 pub struct Tree {
     name: String,
-    style: TreeStyle,
     leaves: Vec<String>,
-    branches: Vec<Box<Tree>>,
+    branches: Vec<Tree>,
 }
 
 pub struct Table {
@@ -84,14 +73,14 @@ pub struct Table {
 }
 
 #[non_exhaustive]
-pub enum Token {
-    Title(TextSpan),
-    SubTitle(TextSpan),
-    Section(TextSpan),
-    Text(TextSpan),
-    SubText(TextSpan),
-    Quote(TextSpan),
-    Alert(Alert),
+pub enum DocumentToken {
+    Title(TextBody),
+    SubTitle(TextBody),
+    Section(TextBody),
+    Text(TextBody),
+    SubText(TextBody),
+    Quote(TextBody),
+    Alert(AlertToken),
     LinkBtn(String),
     Code(String),
     Table(Table),
@@ -99,13 +88,42 @@ pub enum Token {
     List(List),
     YouTubeEmbed(String),
     VideoEmbed(String),
-    Image(String), //also covers gifs
-    //injects are not going to be tokenized, they are going to be pre-agregated pre-tokenization
+    Image(String), //covers gifs
+}
+
+
+//unsure if this should be non-exhaustive or not, will have to research the current discourse around the age signal laws more
+pub enum Rating {
+    UnderOr13, // 0 - 13
+    Rng14_15,  // 14 - 15
+    Rng16_17,  // 16 - 17
+    OverOr18,  // 18+
+}
+
+pub struct DocumentMeta {
+    pub title: Option<String>,
+    pub subtitle: Option<String>,
+    pub date: Option<String>,
+    pub authorship: Option<Vec<String>>,
+    pub tags: Option<Vec<String>>,
+    pub rating: Option<Rating>,
+    pub vars: Option<HashMap<String, String>>
 }
 
 pub struct Document {
-    meta:Meta,
-    tokens:Vec<Token>
+    meta: DocumentMeta,
+    data: String
 }
 
-pub struct Vars(HashMap<String, String>);
+impl Document {
+    pub fn from_str(raw: &str) -> Self {
+        todo!() // use graymatter to deserialize front matter
+    }
+    pub fn interpolated(&self) -> String {
+        todo!() // use handlebars to get back interpolated
+    }
+}
+// i need to pause here and consider if i want to continue with this draft
+// i need to make sure i am not overdoing it
+// this is just for tails and markdown
+// 
